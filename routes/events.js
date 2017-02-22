af8d7a494bee79ef8bf899ec7c7069f630a573d5
Woh Shon <wohshon@gmail.com>
2017-02-22 18:24:31 +0800

    update port

--- routes/events.js
+++ routes/events.js
@@ -15,7 +15,7 @@ router.get('/', function(req, res, next) {
 function callServer (payload) {
 //var socket = require('socket.io-client')('http://192.168.223.130:3001');
 var io = require('socket.io-client');
-var target=process.env.TARGET || 'http://localhost:8082';
+var target=process.env.TARGET || 'http://localhost:8080';
 var socket = io.connect(target, {reconnect: true});
 //var socket = io.connect("http://socket-rest-wohshon.44fs.preview.openshiftapps.com/", {reconnect: true});
 socket.on('connect', function(){
